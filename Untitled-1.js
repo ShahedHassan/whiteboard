e73907970c66cea6bdce70f9b83b6af62ab0464d
Shade Hassan <Shade@MacBook-Pro-8.local>
2022-06-16 10:56:54 -0500

    poosh

---
+++ Untitled-1.js
@@ -0,0 +1,14 @@
+//Whiteboarding
+//please spare me
+
+//name a string
+//string.reverse
+//console.log
+
+function Reverse(){
+    var random = "Hello";
+    return random.reverse();
+
+}
+
+console.log(Reverse());
